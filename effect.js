df82330947e7a4bb38684fb021ecb3a7856b7d33
PraveenVvs <43289901+PraveenVvs@users.noreply.github.com>
2021-03-11 16:42:19 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,15 +7,15 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
-			$('#b88').animate({top:240, left: vw+300},500);
-			$('#b99').animate({top:240, left: vw+350},500);
+			$('#b11').animate({top:240, left: vw-450},500);
+		$('#b22').animate({top:240, left: vw-350},500);
+		$('#b33').animate({top:240, left: vw-250},500);
+		$('#b44').animate({top:240, left: vw-150},500);
+		$('#b55').animate({top:240, left: vw-50},500);
+		$('#b66').animate({top:240, left: vw+50},500);
+		$('#b77').animate({top:240, left: vw+150},500);
+		$('#b88').animate({top:240, left: vw+250},500);
+		$('#b99').animate({top:240, left: vw+350},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -170,15 +170,15 @@ $('document').ready(function(){
 		$('#b7').attr('id','b77')
 		$('#b8').attr('id','b88')
 		$('#b9').attr('id','b99')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
-		$('#b88').animate({top:240, left: vw+350},500);
-		$('#b99').animate({top:240, left: vw+450},500);
+		$('#b11').animate({top:240, left: vw-450},500);
+		$('#b22').animate({top:240, left: vw-350},500);
+		$('#b33').animate({top:240, left: vw-250},500);
+		$('#b44').animate({top:240, left: vw-150},500);
+		$('#b55').animate({top:240, left: vw-50},500);
+		$('#b66').animate({top:240, left: vw+50},500);
+		$('#b77').animate({top:240, left: vw+150},500);
+		$('#b88').animate({top:240, left: vw+250},500);
+		$('#b99').animate({top:240, left: vw+350},500);
 
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
